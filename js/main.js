7fe548e72e084fd767db5dd116ecc44985a4340f
marocena26 <mariaelena.arocena@gmail.com>
2022-10-31 09:04:58 +0100

    created HTML structure with class css and class js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*### ¿Qué tenemos que hacer antes de empezar a programar?
+
+1. Tener nuestra lista de tareas.
+2. Clonar el repositorio.
+3. Leer bien el enunciado y preguntar las dudas.
+4. Crear el HTML.
+5. Crear el CSS mínimo y necesario (no es prioritario). 
+6. JS: crear las constantes (`document.querySelector-select, button, div, main`).
+7. Escuchar evento sobre el botón. 
+    1. 1er proceso -- pintar en el HTML la cara seleccionada.
+    2. 2do proceso -- generar el número aleatorio.
+    3.  3er proceso -- a partir del número aleatorio pitar un color u otro.*/ 
+
